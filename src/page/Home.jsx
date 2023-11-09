f0312472a982776a693eba54ed79a026cb18f247
myeongwon2 <dnfroge@gmail.com>
2023-11-09 18:38:06 +0900

    'commit'

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
 
   const fetchData = async (url, setStateFunction, limit) => {
     await axios
-      .get(`${process.env.REACT_APP_API + url}`)
+      .get(url)
       .then((res) => {
         const items = limit ? res.data.slice(0, 8) : res.data;
         setStateFunction(items);
@@ -26,9 +26,9 @@ function Home() {
 
   useEffect(() => {
     dispatch(trueLoading());
-    fetchData("/carousel/item", setImage, false);
-    fetchData("/best/item", setBest, true);
-    fetchData("/new/item", setNewItem, true);
+    fetchData(`${process.env.REACT_APP_API}/carousel/item`, setImage, false);
+    fetchData(`${process.env.REACT_APP_API}/best/item`, setBest, true);
+    fetchData(`${process.env.REACT_APP_API}/new/item`, setNewItem, true);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
